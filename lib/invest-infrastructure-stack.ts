61b16eeb3352cbf843099b108395128ae9ede188
AndreyKarapetyan <andrey.karapetyan2000@mail.ru>
2023-02-19 05:47:32 +0400

    Initial attempt with lambda and api gateway

--- lib/invest-infrastructure-stack.ts
+++ lib/invest-infrastructure-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class InvestInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InvestInfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new NodejsFunction(this, 'MyFunction', {
+      entry: path.join(__dirname, '../invest-be/apps/myapp/src/main.ts'),
+      handler: 'handler',
+      runtime: Runtime.NODEJS_18_X,
+      bundling: {
+        externalModules: ['@nestjs/microservices', '@nestjs/websockets'],
+      },
+    });
+    new LambdaRestApi(this, 'LambdaDeployment', {
+      handler,
+      proxy: true,
+    });
   }
 }
